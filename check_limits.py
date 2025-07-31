64efd3900aa397b3695797d3a6983930ca524cfa
Pradhap <49062084+inventorpradhap@users.noreply.github.com>
2025-07-31 16:41:04 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,23 @@
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+if temperature < 0 or temperature > 45:
+        return False, 'Temperature is out of range!'
+    if soc < 20 or soc > 80:
+        return False, 'State of Charge is out of range!'
+    if charge_rate > 0.8:
+        return False, 'Charge rate is out of range!'
 
-  return True
-
+    return True, 'Battery is okay.'
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    # Test cases
+    test_cases = [
+        (25, 70, 0.7),  # Expected: (True, 'Battery is okay.')
+        (50, 85, 0),    # Expected: (False, 'Temperature is out of range!')
+        (25, 10, 0.7),  # Expected: (False, 'State of Charge is out of range!')
+        (25, 70, 0.9),   # Expected: (False, 'Charge rate is out of range!')
+    ]
+
+    for temp, soc, rate in test_cases:
+        result, message = battery_is_ok(temp, soc, rate)
+        print('Testing with Temp: {temp}, SOC: {soc}, Charge Rate: {rate} -> Result: {result}, Message: "{message}"')
